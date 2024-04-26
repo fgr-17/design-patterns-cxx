2267d70584f2c4a450f37fcf25fb2d9b9107e190
Federico Roux <rouxfederico@gmail.com>
2024-04-26 18:51:41 -0300

    basic structure of lesson src

---
+++ src/section_1/ch3_single_resp/ch3_single_resp.cpp
@@ -0,0 +1,42 @@
+/**
+ *    @file main.cpp
+ *    @brief main file
+ *    @author rouxfederico@gmail.com
+ */
+
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define BOLD        "\e[1m"
+#define NON_BOLD    "\e[0m"
+
+struct Journal {
+    string title;
+    vector<string> entries;
+
+    Journal(const string &title): title(title) {}
+
+    void add_entry(const string& entry) {
+        static int count = 1;
+        entries.push_back(to_string(count) + ": " + entry);
+    }
+};
+
+
+/**
+ *   @fn main
+ *   @brief main program
+ */
+
+int main(void) {
+
+  cout << "\e[1mDesign Patterns in Modern C++\e[0m" << endl;
+  cout << "\e[1mSection 2:\e[0m SOLID Design Principles" << endl;
+  cout << "\e[1mChapter 3:\e[0m Single Responsibility Principle" << endl;
+
+  return 0;
+}

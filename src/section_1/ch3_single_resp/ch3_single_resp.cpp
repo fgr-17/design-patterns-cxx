6d3f623cf4fe21af71c7a31010e178b6a8816a63
Federico Roux <rouxfederico@gmail.com>
2024-04-26 18:55:28 -0300

    instance to struct created

--- src/section_1/ch3_single_resp/ch3_single_resp.cpp
+++ src/section_1/ch3_single_resp/ch3_single_resp.cpp
@@ -34,6 +34,10 @@ struct Journal {
 
 int main(void) {
 
+  Journal journal{"Dear diary"};
+  journal.add_entry("I ate a bug");
+  journal.add_entry("I cried today");
+
   cout << "\e[1mDesign Patterns in Modern C++\e[0m" << endl;
   cout << "\e[1mSection 2:\e[0m SOLID Design Principles" << endl;
   cout << "\e[1mChapter 3:\e[0m Single Responsibility Principle" << endl;

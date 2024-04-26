0e7a84dff58fc5603f0f357aecb2acdc5550b9b2
Federico Roux <rouxfederico@gmail.com>
2024-04-26 19:01:13 -0300

    finished section-1 ch3

--- src/section_1/ch3_single_resp/ch3_single_resp.cpp
+++ src/section_1/ch3_single_resp/ch3_single_resp.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -15,17 +16,38 @@ using namespace std;
 #define NON_BOLD    "\e[0m"
 
 struct Journal {
-    string title;
-    vector<string> entries;
+  
+  string title;
+  vector<string> entries;
 
-    Journal(const string &title): title(title) {}
+  Journal(const string &title): title(title) {}
 
-    void add_entry(const string& entry) {
-        static int count = 1;
-        entries.push_back(to_string(count) + ": " + entry);
-    }
+  void add_entry(const string& entry) {
+      static int count = 1;
+      entries.push_back(to_string(count) + ": " + entry);
+  }
+
+  // this method would violate single resp principle:
+  // void save(const string& filename) {
+  //   ofstream ofs(filename);
+  //   for (auto& e: entries) {
+  //     ofs << e << endl;
+  //   }
+  // }
 };
 
+
+struct PersistenceManager {
+
+  // in this site, it shouldn't break the single resp principle:
+  void save(const Journal& j, const string& filename) {
+    ofstream ofs(filename);
+    for (auto& e: j.entries) {
+      ofs << e << endl;
+    }
+  }
+
+};
 
 /**
  *   @fn main
@@ -37,6 +59,10 @@ int main(void) {
   Journal journal{"Dear diary"};
   journal.add_entry("I ate a bug");
   journal.add_entry("I cried today");
+
+  PersistenceManager pm;
+
+  pm.save(journal, "diary.txt");
 
   cout << "\e[1mDesign Patterns in Modern C++\e[0m" << endl;
   cout << "\e[1mSection 2:\e[0m SOLID Design Principles" << endl;

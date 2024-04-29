599baa00ea8e9b77df9958ce9cf73d94063cca1c
Federico Roux <rouxfederico@gmail.com>
2024-04-29 11:18:04 -0300

    s1ch4 baseline added

---
+++ src/section_1/ch4_open_closed/c4_open_closed.cpp
@@ -0,0 +1,32 @@
+/**
+ *    @file main.cpp
+ *    @brief main file
+ *    @author rouxfederico@gmail.com
+ */
+
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+#define BOLD        "\e[1m"
+#define NON_BOLD    "\e[0m"
+
+
+
+/**
+ *   @fn main
+ *   @brief main program
+ */
+
+int main(void) {
+
+  cout << "\e[1mDesign Patterns in Modern C++\e[0m" << endl;
+  cout << "\e[1mSection 2:\e[0m SOLID Design Principles" << endl;
+  cout << "\e[1mChapter 4:\e[0m Open-Closed Principle" << endl;
+
+  return 0;
+}

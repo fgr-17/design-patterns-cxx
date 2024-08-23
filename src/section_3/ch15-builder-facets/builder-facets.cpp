1554f0ca6874c53a72648edcb65edc7b2579d948
Federico Roux <rouxfederico@gmail.com>
2024-08-23 09:54:52 -0300

    ch15 builder facets template

---
+++ src/section_3/ch15-builder-facets/builder-facets.cpp
@@ -0,0 +1,45 @@
+/**
+ *    @file builder.cpp
+ *    @brief Chapter 13: Groovy-Style Builders
+ *    @brief
+ *    @author rouxfederico@gmail.com
+ *
+ */
+
+#include <iostream>
+// // #include <cstdio>
+// #include <ostream>   // IWYU pragma: keep
+// #include <sstream>   // IWYU pragma: keep
+// #include <string>
+// #include <vector>
+// // #include <fstream>
+// // #include <tuple>
+// // #include <sstream>
+// // #include <memory>
+// #include <utility>
+// #include <initializer_list>
+// #include <type_traits>
+
+
+/**
+ *   @fn printTitle
+ *   @brief class header
+ */
+
+static int printTitle() {
+  std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+  std::cout << "\e[1mSection 3:\e[0m Builder" << std::endl;
+  std::cout << "\e[1mChapter 15:\e[0m Builder Facets" << std::endl;
+  return 0;
+}
+
+/**
+ *   @fn main
+ *   @brief main program
+ */
+
+int main() {
+    printTitle();
+
+    return 0;
+}

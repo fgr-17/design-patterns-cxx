86ecfad83ba738b40173f2dffbe9fd74c49944a3
Federico Roux <rouxfederico@gmail.com>
2024-08-23 09:51:03 -0300

    completed groovy-stlye builder

--- src/section_3/ch14-groovy-style-builder/groovy-style-builder.cpp
+++ src/section_3/ch14-groovy-style-builder/groovy-style-builder.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 // #include <cstdio>
+#include <ostream>   // IWYU pragma: keep
+#include <sstream>   // IWYU pragma: keep
 #include <string>
 #include <vector>
 // #include <fstream>
@@ -15,12 +17,56 @@
 // #include <sstream>
 // #include <memory>
 #include <utility>
+#include <initializer_list>
+#include <type_traits>
 
 
 struct Tag {
-  std::string name, text;
-  std::vector<Tag> children;
-  std::vector<std::pair<std::string, std::string>> attributes;
+    std::string name, text;
+    std::vector<Tag> children;
+    std::vector<std::pair<std::string, std::string> > attributes;
+
+    friend std::ostream& operator<<(std::ostream& os, const Tag& tag) {
+        os << "<" << tag.name;
+
+        for (const auto& att : tag.attributes)
+            os << " " << att.first << "=\"" << att.second << "\"";
+
+        if (tag.children.size() == 0 && tag.text.length() == 0) {
+            os << "/>" << std::endl;
+        } else {
+            os << ">" << std::endl;
+
+            if (tag.text.length())
+            os << tag.text << std::endl;
+
+        for (const auto&child : tag.children)
+            os << child;
+
+        os << "</" << tag.name << ">" << std::endl;
+        }
+
+    return os;
+  }
+
+ protected:
+    Tag(const std::string name, const std::string text) : name(std::move(name)), text(std::move(text)) {}
+    Tag(const std::string name, const std::vector<Tag> children) : name(std::move(name)), children(std::move(children)) {}
+};
+
+/**
+ * @brief paragraph
+ */
+
+struct P : Tag {
+    explicit P(const std::string text) : Tag("p", std::move(text)) {}
+    P(std::initializer_list<Tag> children) : Tag{"p", std::move(children)} {}
+};
+
+struct IMG : Tag {
+    explicit IMG(const std::string url) : Tag{"img", ""} {
+        attributes.emplace_back(std::make_pair("src", std::move(url)));
+    }
 };
 
 
@@ -42,7 +88,16 @@ static int printTitle() {
  */
 
 int main() {
-  printTitle();
+    printTitle();
 
-  return 0;
+    std::cout <<
+
+        P {
+            IMG { "http://image-source.com/image.png"}
+        }
+
+        << std::endl;
+
+
+    return 0;
 }

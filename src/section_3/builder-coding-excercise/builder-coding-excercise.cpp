79688fbed2598506f4c993f9968cb27e5d3a359b
Federico Roux <rouxfederico@gmail.com>
2024-08-24 20:34:26 -0300

    coding excercise passed

--- src/section_3/builder-coding-excercise/builder-coding-excercise.cpp
+++ src/section_3/builder-coding-excercise/builder-coding-excercise.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 // // #include <cstdio>
-// #include <ostream>   // IWYU pragma: keep
-// #include <sstream>   // IWYU pragma: keep
+#include <ostream>   // IWYU pragma: keep
+#include <sstream>   // IWYU pragma: keep
 // #include <string>
 // #include <vector>
 // // #include <fstream>
@@ -20,6 +20,33 @@
 // #include <initializer_list>
 // #include <type_traits>
 
+#include <string>
+#include <utility>
+#include <vector>
+
+class CodeBuilder {
+    using field = std::pair<std::string, std::string>;
+    std::vector<field> fields_;
+    std::string className_;
+ public:
+  explicit CodeBuilder(const std::string className) : className_{std::move(className)} {}
+
+  CodeBuilder& addField(const std::string name, const std::string type) {
+    fields_.emplace_back(std::make_pair(type, name));
+    return *this;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const CodeBuilder& obj) {
+      os << "class " << obj.className_ << std::endl;
+      os << "{" << std::endl;
+
+      for (auto& field : obj.fields_) {
+          os << "  " << field.first << " " << field.second << ";" << std::endl;
+      }
+      os << "};" << std::endl;
+      return os;
+  }
+};
 
 /**
  *   @fn printTitle
@@ -29,7 +56,7 @@
 static int printTitle() {
   std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
   std::cout << "\e[1mSection 3:\e[0m Builder" << std::endl;
-  std::cout << "\e[1mCoding exercise" << std::endl;
+  std::cout << "\e[1mCoding exercise\e[0m" << std::endl;
   return 0;
 }
 
@@ -40,6 +67,7 @@ static int printTitle() {
 
 int main() {
     printTitle();
-
+    auto cb = CodeBuilder{"Person"}.addField("name", "string").addField("age", "int");
+    std::cout << cb;
     return 0;
 }

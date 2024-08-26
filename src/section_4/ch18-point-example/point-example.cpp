66deadec122d31106d7d0f9ba48172d920815eeb
Federico Roux <rouxfederico@gmail.com>
2024-08-26 13:09:52 -0300

    point example ok

--- src/section_4/ch18-point-example/point-example.cpp
+++ src/section_4/ch18-point-example/point-example.cpp
@@ -7,6 +7,42 @@
  */
 
 #include <iostream>
+#include <cmath>        // IWYU pragma: keep
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+
+
+enum class PointType {
+    cartesian,
+    polar
+};
+
+
+struct Point {
+    // initialize with cartesian coordinates
+    // Point(float x, float y) : x(x), y(y) {}
+
+    // initialize with polar coordinates --> ERROR
+    // Point(float rho, float theta) : x(x), y(y) {}
+
+    // a and b aren't good names, they don't represent anything useful
+    Point(float a, float b, PointType type = PointType::cartesian) {
+        if (type == PointType::cartesian) {
+            x = a;
+            y = b;
+        } else {
+            x = a*std::cos(b);
+            y = a*std::sin(b);
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Point&p) {
+        os << "P(x, y) = (" << p.x << ", " << p.y << ")";
+        return os;
+    }
+
+    float x, y;
+};
 
 /**
  *   @fn printTitle
@@ -27,6 +63,12 @@ static int printTitle() {
 
 int main() {
     printTitle();
+
+    const float x = 3.3, y = 3.2;
+    const float rho = 4.1, th = 0.3;
+
+    std::cout << Point(x, y) << std::endl;
+    std::cout << Point(rho, th, PointType::polar) << std::endl;
 
     return 0;
 }

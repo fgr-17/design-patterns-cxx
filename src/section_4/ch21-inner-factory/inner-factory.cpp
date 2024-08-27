31ce3f6d1c7c926f3ddc4a6564fc6345016953be
Federico Roux <rouxfederico@gmail.com>
2024-08-27 15:02:12 -0300

    inner factory completed

--- src/section_4/ch21-inner-factory/inner-factory.cpp
+++ src/section_4/ch21-inner-factory/inner-factory.cpp
@@ -1,5 +1,5 @@
 /**
- *    @file inner-factory.cpp
+ *    @file factory.cpp
  *    @brief Chapter 13: Groovy-Style Builders
  *    @brief
  *    @author rouxfederico@gmail.com
@@ -24,33 +24,40 @@ enum class PointType {
  */
 
 class Point {
-    friend struct PointFactory;                 // hits on OCP
-    Point(float x, float y) : x(x), y(y) {}
+    float x_, y_;
+    Point(float x, float y) : x_(x), y_(y) {}
+
+    // option 2: private factory and public static instance of this
+    struct Factory {
+        static Point newCartesian(float x, float y) {
+            return {x, y};
+        }
+
+        static Point newPolar(float r, float theta) {
+            return {r*std::cos(theta), r*std::sin(theta)};
+        }
+    };
+
 
  public:
-    float x, y;
+    // option 1: public factory inside the base class
+    // struct Factory {
+    //     static Point newCartesian(float x, float y) {
+    //         return {x, y};
+    //     }
+
+    //     static Point newPolar(float r, float theta) {
+    //         return {r*std::cos(theta), r*std::sin(theta)};
+    //     }
+    // };
+
+    const static Factory factory;
 
     friend std::ostream& operator<<(std::ostream& os, const Point&p) {
-        os << "P(x, y) = (" << p.x << ", " << p.y << ")";
+        os << "P(x, y) = (" << p.x_ << ", " << p.y_ << ")";
         return os;
     }
 };
-
-/**
- *   @fn PointFactory
- *   @brief
- */
-
-struct PointFactory {
-    static Point newCartesian(float x, float y) {
-        return {x, y};
-    }
-
-    static Point newPolar(float r, float theta) {
-        return {r*std::cos(theta), r*std::sin(theta)};
-    }
-};
-
 
 /**
  *   @fn printTitle
@@ -75,8 +82,13 @@ int main() {
     const float x = 3.3, y = 3.2;
     const float rho = 5.0, th = M_PI_4;
 
-    auto p0 = PointFactory::newCartesian(x, y);
-    auto p1 = PointFactory::newPolar(rho, th);
+    // option 1: public factories
+    // auto p0 = Point::Factory::newCartesian(x, y);
+    // auto p1 = Point::Factory::newPolar(rho, th);
+    //
+    // option 2: instantiate factory class as static member
+    auto p0 = Point::factory.newCartesian(x, y);
+    auto p1 = Point::factory.newPolar(rho, th);
 
     std::cout << p0 << std::endl;
     std::cout << p1 << std::endl;

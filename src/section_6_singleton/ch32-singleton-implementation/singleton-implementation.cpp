f58c67ca0bcf0138ab5549181907b5a733f566d5
Federico Roux <rouxfederico@gmail.com>
2024-10-24 08:29:47 -0300

    adding capitals and reading file ok

--- src/section_6_singleton/ch32-singleton-implementation/singleton-implementation.cpp
+++ src/section_6_singleton/ch32-singleton-implementation/singleton-implementation.cpp
@@ -4,12 +4,37 @@
  *    @author rouxfederico@gmail.com
  */
 
+#include <fstream>      // IWYU pragma: keep
 #include <iostream>
-#include <cmath>        // IWYU pragma: keep
-#include <memory>
+#include <map>
 #include <ostream>      // IWYU pragma: keep
 #include <sstream>      // IWYU pragma: keep
+#include <string>
 #include <utility>
+
+
+class SingletonDatabase {
+ public:
+    SingletonDatabase() {
+        std::cout << "Initializing db" << std::endl;
+        std::ifstream ifs("/workspace/src/section_6_singleton/ch32-singleton-implementation/capitals");
+
+        std::string cityLine, populationLine;
+
+        while (getline(ifs, cityLine)) {
+            getline(ifs, populationLine);
+            int pop = std::stoi(populationLine);
+            capitals[cityLine] = pop;
+        }
+    }
+    std::map<std::string, int> capitals;
+
+    void print() {
+        for (auto const&capital : capitals) {
+            std::cout << capital.first << ": " << capital.second << std::endl;
+        }
+    }
+};
 
 
 /**
@@ -33,6 +58,9 @@ static int printTitle() {
 
 int main() {
     printTitle();
+
+    SingletonDatabase db;
+    db.print();
 
     return 0;
 }

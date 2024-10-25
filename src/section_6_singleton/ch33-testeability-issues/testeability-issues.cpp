90065a692e2fc8885c42fe79336c1f548999e8ef
Federico Roux <rouxfederico@gmail.com>
2024-10-25 08:58:27 -0300

    ch33 template ok

---
+++ src/section_6_singleton/ch33-testeability-issues/testeability-issues.cpp
@@ -0,0 +1,87 @@
+/**
+ *    @file testeability-issues.cpp
+ *    @brief Chapter 33: Testeability Issues
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <fstream>      // IWYU pragma: keep
+#include <iostream>
+#include <map>
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+#include <string>
+#include <utility>
+
+
+class SingletonDatabase {
+    SingletonDatabase() {
+        std::cout << "Initializing db" << std::endl;
+        std::ifstream ifs("/workspace/src/section_6_singleton/ch32-singleton-implementation/capitals");
+
+        std::string cityLine, populationLine;
+
+        while (getline(ifs, cityLine)) {
+            getline(ifs, populationLine);
+            int pop = std::stoi(populationLine);
+            capitals_[cityLine] = pop;
+        }
+    }
+    std::map<std::string, int> capitals_;
+
+ public:
+    // deleting copy constructor and assignment:
+    SingletonDatabase(SingletonDatabase const&) = delete;
+    SingletonDatabase& operator=(SingletonDatabase const&) = delete;
+
+    // for clang-tidy compliance:
+    SingletonDatabase(SingletonDatabase&&) = default;
+    SingletonDatabase& operator=(SingletonDatabase&&) = default;
+    ~SingletonDatabase() = default;
+
+
+    static SingletonDatabase& get() {
+        static SingletonDatabase db;
+        return db;
+    }
+
+    int getPopulation(const std::string& city) {
+        return capitals_[city];
+    }
+
+
+    void print() {
+        for (auto const&capital : capitals_) {
+            std::cout << capital.first << ": " << capital.second << std::endl;
+        }
+    }
+};
+
+
+/**
+ *   @fn printTitle
+ *   @brief chapter title
+ */
+
+static int printTitle() {
+  std::cout << "=========================================" << std::endl;
+  std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+  std::cout << "\e[1mSection 6:\e[0m Singleton" << std::endl;
+  std::cout << "\e[1mChapter 33:\e[0m Testeability Issues" << std::endl;
+  std::cout << "=========================================" << std::endl;
+  return 0;
+}
+
+/**
+ *   @fn main
+ *   @brief Singleton Implementation
+ */
+
+int main() {
+    printTitle();
+
+    std::string city = "Tokyo";
+
+    SingletonDatabase::get().print();
+    std::cout << "\n" << city << " has population of " << SingletonDatabase::get().getPopulation(city) << std::endl;
+    return 0;
+}

23155b1b1b7ee8a366a8e7c7353fb724c241d062
Federico Roux <rouxfederico@gmail.com>
2024-10-31 11:21:54 -0300

    template for CH34 Singleton in Dependency Injection

---
+++ src/section_6_singleton/ch34-singleton-dep-injection/singleton-database.cpp
@@ -0,0 +1,52 @@
+/**
+ *    @file singleton-database.cpp
+ *    @brief Chapter 33: Testeability Issues
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <fstream>      // IWYU pragma: keep
+#include <iostream>
+#include <map>
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "singleton-database.h"
+
+SingletonDatabase::SingletonDatabase() {
+    std::cout << "Initializing db" << std::endl;
+    std::ifstream ifs("/workspace/src/section_6_singleton/ch32-singleton-implementation/capitals");
+
+    std::string cityLine, populationLine;
+
+    while (getline(ifs, cityLine)) {
+        getline(ifs, populationLine);
+        int pop = std::stoi(populationLine);
+        capitals_[cityLine] = pop;
+    }
+}
+
+SingletonDatabase& SingletonDatabase::get() {
+    static SingletonDatabase db;
+    return db;
+}
+
+int SingletonDatabase::getPopulation(const std::string& city) {
+    return capitals_[city];
+}
+
+void SingletonDatabase::print() {
+    for (auto const&capital : capitals_) {
+        std::cout << capital.first << ": " << capital.second << std::endl;
+    }
+}
+
+int SingletonRecordFinder::totalPopulation(std::vector<std::string> cities) {
+    int result{0};
+    for (auto& city : cities) {
+        result += SingletonDatabase::get().getPopulation(city);
+    }
+    return result;;
+}

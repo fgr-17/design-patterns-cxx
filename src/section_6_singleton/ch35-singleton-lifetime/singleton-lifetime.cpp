e5a593b4c7a8a1b8e13469ae43ecbcfd9514ebe8
froux <rouxfederico@gmail.com>
2024-12-13 09:16:32 -0300

    ch35 template ok

---
+++ src/section_6_singleton/ch35-singleton-lifetime/singleton-lifetime.cpp
@@ -0,0 +1,35 @@
+/**
+ *    @file testeability-issues.cpp
+ *    @brief Chapter 35: Singleton Lifetime in DI Container
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <fstream>      // IWYU pragma: keep
+#include <iostream>
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+#include <string>
+
+/**
+ *   @fn printTitle
+ *   @brief chapter title
+ */
+
+static int printTitle() {
+  std::cout << "=========================================" << std::endl;
+  std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+  std::cout << "\e[1mSection 6:\e[0m Singleton" << std::endl;
+  std::cout << "\e[1mChapter 35:\e[0m Singleton Lifetime in DI Container" << std::endl;
+  std::cout << "=========================================" << std::endl;
+  return 0;
+}
+
+/**
+ *   @fn main
+ *   @brief Singleton Implementation
+ */
+
+int main() {
+    printTitle();
+    return 0;
+}

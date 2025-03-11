987967db44629604efd49c3b1ddbd8c446a25514
froux <rouxfederico@gmail.com>
2025-03-11 10:29:28 -0300

    singleton tester tested online

--- src/section_6_singleton/coding-excercise/coding-excercise.cpp
+++ src/section_6_singleton/coding-excercise/coding-excercise.cpp
@@ -1,13 +1,30 @@
 /**
  *    @file multiton .cpp
- *    @brief Chapter 37: Multiton
- *    @author rouxfederico@gmail.com
+ *    @brief Singleton Coding Exercise
+
+Implementing a singleton is a bit too easy, so you've got a different challenge. 
+You are given the function SingletonTester::is_singleton()  defined below. This function takes a factory, 
+and needs to return true  or false  depending on whether that factory produces singletons.
+This one's actually easy. Ask yourself: what traits do two 'instances' of a singleton have in common?
+
+*    @author rouxfederico@gmail.com
  */
 
 #include <fstream>      // IWYU pragma: keep
 #include <iostream>
 #include <ostream>      // IWYU pragma: keep
 #include <sstream>      // IWYU pragma: keep
+#include <functional>
+
+
+struct SingletonTester {
+  template <typename T>
+  bool isSingleton(std::function<T*()> factory) {
+    T* instance1 = factory();
+    T* instance2 = factory();
+    return (instance1 == instance2);
+  }
+};
 
 /**
  *   @fn printTitle

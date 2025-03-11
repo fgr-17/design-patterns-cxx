8344dd77d8060ba1c66573964e35a07fbaa4ff9e
froux <rouxfederico@gmail.com>
2025-03-11 10:27:14 -0300

    coding excercise template

---
+++ src/section_6_singleton/coding-excercise/coding-excercise.cpp
@@ -0,0 +1,34 @@
+/**
+ *    @file multiton .cpp
+ *    @brief Chapter 37: Multiton
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <fstream>      // IWYU pragma: keep
+#include <iostream>
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+
+/**
+ *   @fn printTitle
+ *   @brief chapter title
+ */
+
+static int printTitle() {
+  std::cout << "=========================================" << std::endl;
+  std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+  std::cout << "\e[1mSection 6:\e[0m Singleton" << std::endl;
+  std::cout << "\e[1mCoding Excercise\e[0m" << std::endl;
+  std::cout << "=========================================" << std::endl;
+  return 0;
+}
+
+/**
+ *   @fn main
+ *   @brief Coding Excercise
+ */
+
+int main() {
+  printTitle();
+  return 0;
+}

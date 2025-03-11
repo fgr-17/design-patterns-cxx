e58b0c96eee3fedf16db6f6106ceff4086ea7e74
froux <rouxfederico@gmail.com>
2025-03-11 09:23:57 -0300

    monostate finished

--- src/section_6_singleton/ch36-monostate/monostate.cpp
+++ src/section_6_singleton/ch36-monostate/monostate.cpp
@@ -9,6 +9,16 @@
 #include <ostream>      // IWYU pragma: keep
 #include <sstream>      // IWYU pragma: keep
 
+class Printer{
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+  static int id;
+
+ public:
+  [[nodiscard]] int getId() const {return id;}
+  void setId(const int val) {id = val;}
+};
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+int Printer::id = 0;
 
 /**
  *   @fn printTitle
@@ -31,5 +41,12 @@ static int printTitle() {
 
 int main() {
     printTitle();
+    const int magicNumber = 6;
+
+    Printer p0, p1;
+
+    p1.setId(magicNumber);
+    std::cout << "p0::id: " << p0.getId() << "\n";
+
     return 0;
 }

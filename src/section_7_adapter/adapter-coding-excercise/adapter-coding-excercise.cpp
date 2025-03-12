959197308c449b671ba77c2b67370b301a839b42
Federico Roux <rouxfederico@gmail.com>
2025-03-12 11:54:57 -0300

    Section 7/adapter (#8)
    * renaming coding excersie, new section
    
    * adapter coding excercise ok

---
+++ src/section_7_adapter/adapter-coding-excercise/adapter-coding-excercise.cpp
@@ -0,0 +1,68 @@
+/**
+ *    @file adapter-coding-excercise .cpp
+ *    @brief Singleton Coding Exercise
+ *    @brief 
+ *    @brief Adapter Coding Exercise
+ *    @brief Here's a very synthetic example for you to try.
+ *    @brief 
+ *    @brief You are given a Rectangle protocol and an extension method on it. 
+ *    @brief Try to define a SquareToRectangleAdapter that adapts the Square  to the Rectangle  interface.
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <fstream>      // IWYU pragma: keep
+#include <iostream>
+#include <ostream>      // IWYU pragma: keep
+#include <sstream>      // IWYU pragma: keep
+
+struct Square {
+  int side{ 0 };
+  explicit Square(const int side) : side(side) {
+  }
+};
+
+struct Rectangle {
+  [[nodiscard]] virtual int width() const = 0;
+  [[nodiscard]] virtual int height() const = 0;
+
+  [[nodiscard]] int area() const {
+    return width() * height();
+  }
+};
+
+struct SquareToRectangleAdapter : Rectangle {
+  explicit SquareToRectangleAdapter(const Square& square) {
+      width_ = square.side;
+      height_ = square.side;
+  }
+
+  int width_;
+  int height_;
+
+  [[nodiscard]] int width() const override { return width_; };
+  [[nodiscard]] int height() const override { return height_; };
+};
+
+/**
+ *   @fn printTitle
+ *   @brief chapter title
+ */
+
+static int printTitle() {
+  std::cout << "=========================================" << std::endl;
+  std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+  std::cout << "\e[1mSection 7:\e[0m Adapter" << std::endl;
+  std::cout << "\e[1mCoding Excercise\e[0m" << std::endl;
+  std::cout << "=========================================" << std::endl;
+  return 0;
+}
+
+/**
+ *   @fn main
+ *   @brief Coding Excercise
+ */
+
+int main() {
+  printTitle();
+  return 0;
+}

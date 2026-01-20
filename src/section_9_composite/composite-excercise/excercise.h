401e07564a8317ed7bfd48058abc831dab3bdc75
Federico Roux <rouxfederico@gmail.com>
2026-01-20 10:32:07 -0300

    [Section 9] Composite: Coding excercise (#17)
    Signed-off-by: Federico Roux <rouxfederico@gmail.com>
    
    stack-info: PR: https://github.com/fgr-17/design-patterns-cxx/pull/17, branch: fgr-17/stack/8

---
+++ src/section_9_composite/composite-excercise/excercise.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <numeric>
+
+
+struct ContainsIntegers {
+  [[nodiscard]] virtual int sum() const = 0;
+};
+
+struct SingleValue: ContainsIntegers {
+  int value{ 0 };
+
+  SingleValue() = default;
+  explicit SingleValue(const int value): value{value} {}
+
+  [[nodiscard]] int sum() const override {
+    return value;
+  }
+};
+
+struct ManyValues : ContainsIntegers, std::vector<int> {
+  void add(const int value) {
+    push_back(value);
+  }
+
+  [[nodiscard]] int sum() const override {
+    return std::accumulate(begin(), end(), 0);
+  }
+};
+
+[[nodiscard]] int sum(const std::vector<ContainsIntegers*>& items);

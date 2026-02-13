b99c97fd3c2914925ec0a688b057cf7b5f17ec75
Federico Roux <rouxfederico@gmail.com>
2026-02-13 12:30:55 +0000

    [Section 16] Interpreter: Ch85 - Building parsers with `Boost.spirit`
    Signed-off-by: Federico Roux <rouxfederico@gmail.com>
    
    stack-info: PR: https://github.com/fgr-17/design-patterns-cxx/pull/58, branch: fgr-17/stack/49

---
+++ src/section_16_interpreter/ch85-parser-boost/parser-boost.cpp
@@ -0,0 +1,23 @@
+/**
+ *    @file parser-boost.cpp
+ *    @brief ch-85 Parser Boost
+ *    @author rouxfederico@gmail.com
+ */
+
+#include <iostream>
+
+static int printTitle() {
+    std::cout << "=========================================" << std::endl;
+    std::cout << "\e[1mDesign Patterns in Modern C++\e[0m" << std::endl;
+    std::cout << "\e[1mSection 16 :\e[0m Interpreter" << std::endl;
+    std::cout << "\e[1mChapter 85 : Parser Boost\e[0m" << std::endl;
+    std::cout << "=========================================" << std::endl;
+    return 0;
+}
+
+int main() {
+    printTitle();
+
+    std::cout << "this chapter is not reproducible in this repository" << std::endl;
+    return 0;
+}

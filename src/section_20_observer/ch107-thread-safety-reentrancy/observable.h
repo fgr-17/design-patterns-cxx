aea2ff81c817f2b69ec9e16914920cb5c5474dfb
Federico Roux <rouxfederico@gmail.com>
2026-03-20 13:06:13 +0000

    [Section 20] Observer: Ch107 - Thread Safety and Reentrancy
    Signed-off-by: Federico Roux <rouxfederico@gmail.com>
    
    stack-info: PR: https://github.com/fgr-17/design-patterns-cxx/pull/82, branch: fgr-17/stack/1

---
+++ src/section_20_observer/ch107-thread-safety-reentrancy/observable.h
@@ -0,0 +1,34 @@
+/**
+ *    @file observable.h
+ *    @brief ch-107 Thread Safety Reentrancy
+ *    @author rouxfederico@gmail.com
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "observer.h"
+
+template <typename T>
+class Observable {
+    std::vector<Observer<T>*> observers_;
+
+   public:
+    void notify(T& source, const std::string& fieldName) {
+        for (auto observer : observers_) {
+            observer->fieldChanged(source, fieldName);
+        }
+    }
+
+    void subscribe(Observer<T>& observer) {
+        observers_.emplace_back(&observer);
+    }
+
+    void unsubscribe(Observer<T>& observer) {
+        // observers_.erase(std::remove(observers_.begin(), observers_.end(), &observer),
+        //                  observers_.end());
+        std::erase(observers_, &observer);
+    }
+};
